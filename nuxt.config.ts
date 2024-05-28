6eb52762a29c3cc55dba43adc492835f0c390958
Exodia <bbextasy@mail.ru>
2024-05-28 05:28:01 +0300

    feat(postcss)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import path from 'node:path'
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  postcss: {
+    // Add plugin names as key and arguments as value
+    // Install them before as dependencies with npm or yarn
+    plugins: {
+      'postcss-nested-ancestors': {},
+      'postcss-nested': {},
+      'postcss-inline-svg': {
+        paths: [path.join(__dirname, './assets/images/')],
+        removeFill: true,
+      },
+      'cssnano': {
+        preset: 'default',
+      },
+    },
+  },
 })
